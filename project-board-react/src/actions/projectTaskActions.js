fc1eb255818c6ca3ac5d576cc45cbf925cf84e4c
Milan Vasic <vaske494@gmail.com>
2019-10-08 18:27:02 +0200

    Handling errors on frontend

--- project-board-react/src/actions/projectTaskActions.js
+++ project-board-react/src/actions/projectTaskActions.js
@@ -1,10 +1,23 @@
 import axios from "axios";
+import { GET_ERRORS } from "./types";
+
+//Paths
+const createTaskPath = "http://localhost:8080/api/board/create";
 
 export const addProjectTask = (project_task, history) => async dispatch => {
-    await axios.post("http://localhost:8080/api/board/create", project_task)
-    .catch(function (error) {
-        console.log(error);
-    });
 
-    history.push("/"); //Redirect to home page
+    try {
+        await axios.post(createTaskPath, project_task);
+        history.push("/"); //Redirect to home page
+        dispatch({
+            type:"GET_ERRORS",
+            payload: {}
+        });
+        
+    } catch (error) {
+        dispatch({
+            type: GET_ERRORS,
+            payload: error.response.data
+        });
+    }
 }
