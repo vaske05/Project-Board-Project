857bce154eec25b851cea321b3acaea33776dd7a
Milan Vasić <vaske494@gmail.com>
2020-06-27 21:53:43 +0200

    Created Proejct domain model class and Project Repository interface

---
+++ ProjectBoard/src/main/java/com/vasic/project_board/repository/ProjectRepository.java
@@ -0,0 +1,15 @@
+package com.vasic.project_board.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vasic.project_board.domain.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long> {
+
+	@Override
+	Iterable<Project> findAllById(Iterable<Long> ids);
+	
+
+}
