8abbbcdcbff12d935fffb169c4426bb7c67456a7
Milan Vasić <vaske494@gmail.com>
2020-06-30 00:28:26 +0200

    Delete project by identifier

--- ProjectBoard/src/main/java/com/vasic/project_board/service/ProjectService.java
+++ ProjectBoard/src/main/java/com/vasic/project_board/service/ProjectService.java
@@ -30,11 +30,20 @@ public class ProjectService {
 
     public Project findProjectByIdentifier(String projectId) {
 
-        Project project = projectRepository.findByProjectIdentifier(projectId);
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
         if(project == null) {
             throw new ProjectIdException("Project ID '" + projectId + "' does not exist");
         }
 
-        return projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        return project;
+    }
+
+    public void deleteProjectByIdentifier(String projectId) {
+        Project project = projectRepository.findByProjectIdentifier(projectId.toUpperCase());
+        if(project==null) {
+            throw new ProjectIdException("Cannot delete Project with ID '" + projectId + "'. This project does not exist");
+        }
+
+        projectRepository.delete(project);
     }
 }
