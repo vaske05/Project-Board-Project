a2696ff46edb22a196b8227b7c9216e6eaa3180d
Milan Vasić <vaske494@gmail.com>
2020-07-06 18:02:03 +0200

    New components

---
+++ project-board-react/src/components/Project/ProjecItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ProjecItem extends Component { //5
+    render() {
+        return (
+            <div>
+                <h1>Project item</h1>
+            </div>
+        )
+    }
+}
