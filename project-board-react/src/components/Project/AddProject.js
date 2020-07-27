da2a65fe566c734c07a3b66c7742424af312d5fa
Milan Vasić <vaske494@gmail.com>
2020-07-27 15:59:41 +0200

    Add Project page - showing fileds error

--- project-board-react/src/components/Project/AddProject.js
+++ project-board-react/src/components/Project/AddProject.js
@@ -1,41 +1,50 @@
-import React, { Component } from 'react';
-import PropTypes from "prop-types"
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { createProject } from "../../actions/projectActions";
+import classnames from "classnames";
 
 class AddProject extends Component {
+  constructor() {
+    super();
 
-    constructor() {
-        super()
+    this.state = {
+      projectName: "",
+      projectIdentifier: "",
+      description: "",
+      startDate: "",
+      endDate: "",
+      errors: {},
+    };
+    this.onChange = this.onChange.bind(this); //binding
+    this.onSubmit = this.onSubmit.bind(this);
+  }
 
-        this.state = {
-            projectName:"",
-            projectIdentifier: "",
-            description:"",
-            startDate:"",
-            endDate:""
-        };
-        this.onChange = this.onChange.bind(this); //binding 
-        this.onSubmit = this.onSubmit.bind(this);
+  //life cycle hooks
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
     }
+  }
 
-    onChange(e) {
-        this.setState({[e.target.name]: e.target.value});
-    }
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
 
-    onSubmit(e) {
-        e.preventDefault(); // turn off reload
-        const newProject = {
-            projectName: this.state.projectName,
-            projectIdentifier: this.state.projectIdentifier,
-            description: this.state.description,
-            startDate: this.state.startDate,
-            endDate: this.state.endDate
-        }
-        this.props.createProject(newProject, this.props.history);
-    }
+  onSubmit(e) {
+    e.preventDefault(); // turn off reload
+    const newProject = {
+      projectName: this.state.projectName,
+      projectIdentifier: this.state.projectIdentifier,
+      description: this.state.description,
+      startDate: this.state.startDate,
+      endDate: this.state.endDate,
+    };
+    this.props.createProject(newProject, this.props.history);
+  }
 
   render() {
+    const { errors } = this.state; // mapping "this.state.errors" to "errors" constant
     return (
       <div className="project">
         <div className="container">
@@ -49,33 +58,49 @@ class AddProject extends Component {
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg "
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.projectName,
+                    })}
                     placeholder="Project Name"
                     name="projectName"
                     value={this.state.projectName}
                     onChange={this.onChange}
                   />
+                  {errors.projectName && (
+                    <div className="invalid-feedback">{errors.projectName}</div>
+                  )}
                 </div>
                 <div className="form-group">
                   <input
                     type="text"
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.projectIdentifier,
+                    })}
                     placeholder="Unique Project ID"
                     name="projectIdentifier"
                     value={this.state.projectIdentifier}
                     onChange={this.onChange}
-
                   />
+                  {errors.projectIdentifier && (
+                    <div className="invalid-feedback">
+                      {errors.projectIdentifier}
+                    </div>
+                  )}
                 </div>
                 {/* disabled for Edit Only!! remove "disabled" for the Create operation */}
                 <div className="form-group">
                   <textarea
-                    className="form-control form-control-lg"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.description,
+                    })}
                     placeholder="Project Description"
                     name="description"
                     value={this.state.description}
                     onChange={this.onChange}
                   ></textarea>
+                  {errors.description && (
+                    <div className="invalid-feedback">{errors.description}</div>
+                  )}
                 </div>
                 <h6>Start Date</h6>
                 <div className="form-group">
@@ -100,7 +125,7 @@ class AddProject extends Component {
 
                 <input
                   type="submit"
-                  className="btn btn-primary btn-block mt-4"
+                  className="btn btn-success btn-block mt-4"
                 />
               </form>
             </div>
@@ -113,13 +138,12 @@ class AddProject extends Component {
 
 AddProject.propTypes = {
   createProject: PropTypes.func.isRequired,
-  //errors: PropTypes.func.isRequired
+  errors: PropTypes.object.isRequired,
 };
 
-/*
-const mapStateToProps = state => ({
-  errors: state.errors
+const mapStateToProps = (state) => ({
+  errors: state.errors,
 });
-*/
 
-export default connect(null, {createProject})(AddProject);
+//Connect React component to a Redux store.
+export default connect(mapStateToProps, { createProject })(AddProject);
