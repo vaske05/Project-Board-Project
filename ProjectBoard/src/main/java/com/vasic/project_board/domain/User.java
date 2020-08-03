d1a18753a5f4dd78007a4e0267e8ceb6a8a93184
Milan Vasić <vaske494@gmail.com>
2020-08-03 23:19:24 +0200

    1) Added fields to User model 2) Created: UserRepository interface and UserService class

--- ProjectBoard/src/main/java/com/vasic/project_board/domain/User.java
+++ ProjectBoard/src/main/java/com/vasic/project_board/domain/User.java
@@ -1,4 +1,102 @@
 package com.vasic.project_board.domain;
 
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Email(message = "Username needs to be an email")
+    @NotBlank(message = "username is required")
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank(message = "Please enter your full name")
+    private String fullName;
+
+    @NotBlank(message = "Password field is required")
+    private String password;
+
+    @Transient
+    private String confirmPassword; // not stored do db
+
+    private Date createdAt;
+    private Date updatedAt;
+
+    // Onetomany with Project
+
+    public User() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getConfirmPassword() {
+        return confirmPassword;
+    }
+
+    public void setConfirmPassword(String confirmPassword) {
+        this.confirmPassword = confirmPassword;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = new Date();
+    }
 }
---
+++ ProjectBoard/src/main/java/com/vasic/project_board/service/UserService.java
@@ -0,0 +1,12 @@
+package com.vasic.project_board.service;
+
+import com.vasic.project_board.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+}
