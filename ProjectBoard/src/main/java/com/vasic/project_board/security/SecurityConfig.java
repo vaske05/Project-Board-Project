b7829748e4ab43da1952566a550ac44ac4322f60
Milan Vasić <vaske494@gmail.com>
2020-08-04 19:16:14 +0200

    Login request, login response, constants, CustomUserDetailsService...

--- ProjectBoard/src/main/java/com/vasic/project_board/security/SecurityConfig.java
+++ ProjectBoard/src/main/java/com/vasic/project_board/security/SecurityConfig.java
@@ -1,12 +1,22 @@
 package com.vasic.project_board.security;
 
+import com.vasic.project_board.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import com.vasic.project_board.security.SecurityConstants;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import static com.vasic.project_board.security.SecurityConstants.*;
+
 
 @Configuration
 @EnableWebSecurity
@@ -16,27 +26,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private JwtAuthenticationEntryPoint unauthorizedHandler;
 
-    private static final String[] PUBLIC_MATCHERS = {
-            /*"/webjars/**",
-            "/css/**",
-            "/js/**",
-            "/images/**",
-            "/",
-            "/about/**",
-            "/contact/**",
-            "/error/**",
-            "/console/**",
-            "/signup",*/
-            /*NEW*/
-            "/",
-            "favicon.ico",
-            "/**/*.png",
-            "/**/*.gif",
-            "/**/*.svg",
-            "/**/*.jpg",
-            "/**/*.html",
-            "/**/*.js"
-    };
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+        authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
+
+    @Override
+    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -48,9 +53,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .headers().frameOptions().sameOrigin() // to enable H2 Database
                 .and()
                 .authorizeRequests()
-                .antMatchers(PUBLIC_MATCHERS)
-                .permitAll()
-                .antMatchers("/api/users/**").permitAll()
+                .antMatchers(PUBLIC_MATCHERS).permitAll()
+                .antMatchers(SIGN_UP_URLS).permitAll()
+                .antMatchers(H2_URL).permitAll()
                 .anyRequest().authenticated();
     }
 }
